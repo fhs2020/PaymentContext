6fe9e96dbed7696459fd91c332b35f1223629c2c
fhs2020 <flavioh007@gmail.com>
2022-04-02 09:24:44 -0300

    Add project files.

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, string document, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+
+            if(firstName.Length == 0)
+                throw new Exception("Nome invalido");
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+        public List<Subscription> Subscriptions { get; set; }
+
+        public void AddSubscription(Subscription subscription)
+            {
+            
+            }
+    }
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var student = new Student ("Flavio", "Sousa", "2656530212", "fhs@gmail.com");
+            
+
+
+
+        }
+    }
+}
